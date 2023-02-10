6595d0774cd23447973e94ff23e00ce5ce4ca76c
teacher <teacher@BE-403->
2023-02-10 20:48:06 +0800

    web

--- src/main/java/controller/BMIServlet.java
+++ src/main/java/controller/BMIServlet.java
@@ -18,17 +18,18 @@ public class BMIServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 1.確認編碼
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html;charset=UTF-8");
-		// 取得參數
+		// 2.取得參數
 		String height = req.getParameter("height");
 		String weight = req.getParameter("weight");
-		// 確認是否有參數?
+		// 3.確認是否有參數?
 		if (height == null || weight == null) {
 			resp.getWriter().print("無參數或參數不正確");
 			return;
 		}
-		// 進行轉型 String -> double
+		// 4.進行轉型 String -> double
 		double h = 0, w = 0;
 		try {
 			h = Double.parseDouble(height);
@@ -37,7 +38,12 @@ public class BMIServlet extends HttpServlet {
 			resp.getWriter().print("請輸入正確數值資料");
 			return;
 		}
-		// 計算 BMI
+		// 5. 資料檢查合理性
+		if(h < 10 || h > 300 || w < 10 || w > 1000) {
+			resp.getWriter().print("資料不正確, 超過合理範圍~~");
+			return;
+		}
+		// 6.計算 BMI
 		double bmi = w / Math.pow(h/100, 2);
 		String output = String.format("身高:%.1f 體重:%.1f BMI:%.2f", h, w, bmi);
 		resp.getWriter().print(output);
