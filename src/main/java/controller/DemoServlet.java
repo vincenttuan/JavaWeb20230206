e17eec2d7802254a18cc60bbcb5195456176af47
teacher <teacher@BE-403->
2023-02-10 19:36:35 +0800

    web

--- src/main/java/controller/DemoServlet.java
+++ src/main/java/controller/DemoServlet.java
@@ -14,12 +14,14 @@ public class DemoServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=utf-8");
 		resp.getWriter().print("執行 doGet() 方法");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=utf-8");
 		resp.getWriter().print("執行 doPost() 方法");
 	}
 	
