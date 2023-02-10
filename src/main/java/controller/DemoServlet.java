2285a933034729c79bba1e80530ce05f33b4e0bf
teacher <teacher@BE-403->
2023-02-10 19:25:03 +0800

    web

---
+++ src/main/java/controller/DemoServlet.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/servlet/demo")
+public class DemoServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("執行 doGet() 方法");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("執行 doPostGet() 方法");
+	}
+	
+}
