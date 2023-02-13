c2eac8e79cbf02c8b60fbb8ac2f20d6e4dbb716b
teacher <teacher@BE-403->
2023-02-13 20:46:49 +0800

    web

--- src/main/java/controller/PersonServlet.java
+++ src/main/java/controller/PersonServlet.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +15,14 @@ public class PersonServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		resp.getWriter().print("person servlet");
+		
+		// 重導程序-建立分派器
+		RequestDispatcher rd = req.getRequestDispatcher("/html/person_result.jsp");
+		// 帶入給 jsp 的資料
+		
+		// 重導
+		rd.forward(req, resp);
+		
 		
 	}
 	
