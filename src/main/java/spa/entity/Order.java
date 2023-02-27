c1321c3067641977ca9fbb538c02bdf6fb4f82e0
teacher <teacher@BE-403->
2023-02-27 21:14:22 +0800

    spa

--- src/main/java/spa/entity/Order.java
+++ src/main/java/spa/entity/Order.java
@@ -2,7 +2,7 @@ package spa.entity;
 
 // 預約訂單
 public class Order {
-	
+	// 欄位屬性
 	private Integer id; // 訂單編號
 	private String userName; // 客戶名稱(登入帳號)
 	private Integer spaId; // 按摩服務編號
@@ -10,6 +10,31 @@ public class Order {
 	private Integer spaPrice ; // 按摩服務價格
 	private Integer masterId; // 師傅編號
 	private String reserve; // 預約日期
+	
+	// 組合
+	private Spa spa;
+	private Master master;
+	private Member member;
+	
+	// 組合 getter/setter
+	public Spa getSpa() {
+		return spa;
+	}
+	public void setSpa(Spa spa) {
+		this.spa = spa;
+	}
+	public Master getMaster() {
+		return master;
+	}
+	public void setMaster(Master master) {
+		this.master = master;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
 	
 	public Integer getId() {
 		return id;
