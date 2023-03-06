4074e886a89db6819c190ec23b9fbe81ec62b9c7
teacher <teacher@BE-403->
2023-03-06 19:38:44 +0800

    web

--- src/main/java/filter/CFilter.java
+++ src/main/java/filter/CFilter.java
@@ -9,7 +9,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter("/servlet/*")
+//@WebFilter("/servlet/*")
 public class CFilter implements Filter {
 
 	@Override
