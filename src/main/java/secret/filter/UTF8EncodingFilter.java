5babc0fbe26987a5858278a1db76bc9429f1bd13
teacher <teacher@BE-403->
2023-03-06 20:49:19 +0800

    web

---
+++ src/main/java/secret/filter/UTF8EncodingFilter.java
@@ -0,0 +1,23 @@
+package secret.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/secret/*")
+public class UTF8EncodingFilter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		req.setCharacterEncoding("UTF-8");
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html;charset=utf-8");
+	}
+	
+}
