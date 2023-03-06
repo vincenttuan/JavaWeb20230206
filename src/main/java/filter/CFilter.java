49479fca489626ab4dcbb5ebb4b121c2ff7a6313
teacher <teacher@BE-403->
2023-03-06 19:18:32 +0800

    filter

---
+++ src/main/java/filter/CFilter.java
@@ -0,0 +1,23 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/servlet/*")
+public class CFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("CBFilter");
+		// 放行
+		chain.doFilter(request, response);
+	}
+
+}
