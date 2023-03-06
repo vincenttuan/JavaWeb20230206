2beef3a3fae3ad354d65bf2bffdac6bed1514875
teacher <teacher@BE-403->
2023-03-06 21:05:45 +0800

    web

--- src/main/java/secret/filter/SecretFilter.java
+++ src/main/java/secret/filter/SecretFilter.java
@@ -3,11 +3,13 @@ package secret.filter;
 import java.io.IOException;
 
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter("/secret/*")
 public class SecretFilter extends HttpFilter {
@@ -16,7 +18,30 @@ public class SecretFilter extends HttpFilter {
 	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		
-		chain.doFilter(req, res);
+		HttpSession session = req.getSession();
+		Object code = session.getAttribute("code");
+		if(code != null && code.toString().equals("1234")) {
+			// 放行
+			chain.doFilter(req, res);
+		} else if(req.getParameter("passCode") != null) { // 有傳參數訪問
+			String passCode = req.getParameter("passCode");
+			if(passCode.equals("1234")) {
+				// 寫入 session
+				session.setAttribute("code", passCode);
+				// 放行
+				chain.doFilter(req, res);
+			} else {
+				// 重導
+				RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/secret/secret.jsp");
+				rd.forward(req, res);
+			}
+		} 
+		else {
+			session.invalidate();
+			// 重導
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/secret/secret.jsp");
+			rd.forward(req, res);
+		}
 		
 	}
 	
