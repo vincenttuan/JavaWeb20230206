261eaeb8b632d5158d6b447177f38724dd02d404
teacher <teacher@BE-403->
2023-03-06 20:36:08 +0800

    web

--- src/main/java/secret/controller/ReportA.java
+++ src/main/java/secret/controller/ReportA.java
@@ -1,5 +1,19 @@
 package secret.controller;
 
-public class ReportA {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/secret/report/a")
+public class ReportA extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("雞蛋批發價漲3元");
+	}
+	
 }
--- src/main/java/secret/controller/ReportB.java
+++ src/main/java/secret/controller/ReportB.java
@@ -1,5 +1,19 @@
 package secret.controller;
 
-public class ReportB {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/secret/report/b")
+public class ReportB extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("ChatGPT 將為你加薪");
+	}
+	
 }
