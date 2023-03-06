94f927d55a41a0c5902db1551a13ef7906e6de79
teacher <teacher@BE-403->
2023-03-06 20:40:37 +0800

    web

--- src/main/java/secret/filter/SecretFilter.java
+++ src/main/java/secret/filter/SecretFilter.java
@@ -1,5 +1,21 @@
 package secret.filter;
 
-public class SecretFilter {
+import java.io.IOException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecretFilter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+		chain.doFilter(req, res);
+		
+	}
+	
 }
