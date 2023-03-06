9cf1aab4418b21bf750d93e0d64bc159cb64a14f
teacher <teacher@BE-403->
2023-03-06 20:42:49 +0800

    web

--- src/main/java/secret/filter/SecretFilter.java
+++ src/main/java/secret/filter/SecretFilter.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebFilter("/secret/*")
 public class SecretFilter extends HttpFilter {
 
 	@Override
