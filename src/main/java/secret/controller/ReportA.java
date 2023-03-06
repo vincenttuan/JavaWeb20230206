56a6f489e67d285c95be6ce3fb3d17933e6a86cc
teacher <teacher@BE-403->
2023-03-06 21:18:35 +0800

    web

--- src/main/java/secret/controller/ReportA.java
+++ src/main/java/secret/controller/ReportA.java
@@ -12,7 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 public class ReportA extends HttpServlet {
 
 	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandler(req, resp);
+	}
+	
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandler(req, resp);
+	}
+	
+	private void doHandler(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.getWriter().println("雞蛋批發價漲3元");
 	}
 	
--- src/main/java/secret/controller/ReportB.java
+++ src/main/java/secret/controller/ReportB.java
@@ -10,9 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/secret/report/b")
 public class ReportB extends HttpServlet {
-
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandler(req, resp);
+	}
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandler(req, resp);
+	}
+	
+	private void doHandler(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.getWriter().println("ChatGPT 將為你加薪為你效力");
 	}
 	
