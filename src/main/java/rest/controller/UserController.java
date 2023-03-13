5770540bb636231eb641b3f64d6a704aea6a7342
teacher <teacher@BE-403->
2023-03-13 19:07:44 +0800

    web

--- src/main/java/rest/controller/UserController.java
+++ src/main/java/rest/controller/UserController.java
@@ -1,5 +1,35 @@
 package rest.controller;
 
-public class UserController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/rest/user/*")
+public class UserController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("doGet()");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("doPost()");
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("doPut()");
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("doDelete()");
+	}
+
+	
 }
