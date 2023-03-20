fb32ce40340e2c1f58976ff6ef408512e86d554d
teacher <teacher@BE-403->
2023-03-20 19:11:43 +0800

    ajax

---
+++ src/main/java/ajax/entity/Employee.java
@@ -0,0 +1,28 @@
+package ajax.entity;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private Integer salary;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getSalary() {
+		return salary;
+	}
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+	
+	
+}
