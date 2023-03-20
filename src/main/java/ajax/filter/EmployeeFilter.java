b422a69d3ec04b07d28c63c6f7bc937f87e015fe
teacher <teacher@BE-403->
2023-03-20 20:58:50 +0800

    ajax

---
+++ src/main/java/ajax/filter/EmployeeFilter.java
@@ -0,0 +1,30 @@
+package ajax.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/ajax/employees/*")
+public class EmployeeFilter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		// 設定編碼
+		req.setCharacterEncoding("UTF-8");
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("application/json");
+		// 設定 no-cache
+		res.setHeader("Cache-Control", "must-revalidate, no-cache, no-store");
+		res.setHeader("Pragma", "no-cache");
+		res.setDateHeader("Expires", -1);
+		
+		chain.doFilter(req, res);
+	}
+	
+}
