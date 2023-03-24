6e6c5b8da95508014d5810a4d353ff7ba40f5812
teacher <teacher@BE-403->
2023-03-24 19:28:32 +0800

    jpa

--- src/main/java/ajax/entity/Employee.java
+++ src/main/java/ajax/entity/Employee.java
@@ -1,18 +1,28 @@
 package ajax.entity;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "name", nullable = false, length = 50)
 	private String name;
+	
+	@Column(name = "salary", nullable = true)
 	private Integer salary;
 	
 	public Employee() {
 		
-	}
-	
-	public Employee(Integer id, String name, Integer salary) {
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
 	}
 	
 	public Integer getId() {
