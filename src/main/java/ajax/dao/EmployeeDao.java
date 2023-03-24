71b0215c0b183d734e5040e4ce1b40d43e8d7d6b
teacher <teacher@BE-403->
2023-03-24 19:56:36 +0800

    jpa

--- src/main/java/ajax/dao/EmployeeDao.java
+++ src/main/java/ajax/dao/EmployeeDao.java
@@ -4,6 +4,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import ajax.entity.Employee;
 
 public class EmployeeDao {
@@ -11,8 +16,11 @@ public class EmployeeDao {
 	private static List<Employee> employees = new CopyOnWriteArrayList<>();
 	private static EmployeeDao _instance = new EmployeeDao();
 	
+	private EntityManagerFactory entityManagerFactory;
+	
 	private EmployeeDao() {
-		
+		// 建立工廠管理器實體 (實務上要配置在 ServletContextListsner 中)
+		entityManagerFactory = Persistence.createEntityManagerFactory("demo");
 	}
 	
 	public static EmployeeDao getInstance() {
@@ -20,6 +28,10 @@ public class EmployeeDao {
 	}
 	
 	public List<Employee> getAllEmployees() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		Query query = entityManager.createQuery("Select employee From Employee employee"); // PQL
+		List<Employee> employees = query.getResultList(); // 將查詢結果傳換成 List
+		entityManager.close(); // 關閉 entityManager
 		return employees;
 	}
 	
@@ -53,5 +65,14 @@ public class EmployeeDao {
 			employees.remove(index);
 		}
 	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		 // 關閉工廠 (實務上要配置在 ServletContextListsner 中)
+		if(entityManagerFactory != null) { 
+			entityManagerFactory.close();
+		}
+	}
+	
 	
 }
