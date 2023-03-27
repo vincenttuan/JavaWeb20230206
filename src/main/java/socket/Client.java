43baf4b22c9c1f3a6ac46164ee98ccdff4944e7a
teacher <teacher@BE-403->
2023-03-27 21:18:00 +0800

    web

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,5 +1,18 @@
 package socket;
 
-public class Client {
+import java.net.Socket;
 
+public class Client {
+	
+	public static void main(String[] args) throws Exception {
+		String hostName = "localhost";
+		int port = 5555;
+		
+		// Client 連到 Server 端
+		Socket socket = new Socket(hostName, port);
+		System.out.println("Client 連到 Server 端成功");
+		
+		socket.close();
+	}
+	
 }
