b9d1552a49d4692e0b9a78e46516892da3d6bbb9
teacher <teacher@BE-403->
2023-03-27 21:25:05 +0800

    web

--- src/main/java/socket/Server.java
+++ src/main/java/socket/Server.java
@@ -1,5 +1,8 @@
 package socket;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,6 +16,18 @@ public class Server {
 		System.out.println("等待 Client 連入");
 		Socket clientSocket = serverSocket.accept(); // 接收到 client 連入資訊
 		System.out.println("Client 連入成功");
+		// 接收 client 端來的資料
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		String inputLine;
+		while ((inputLine = in.readLine()) != null) {
+			out.print(inputLine);
+		}
+		
+		// 關閉 I/O
+		out.close();
+		in.close();
 		
 		// 關閉連線
 		clientSocket.close();
